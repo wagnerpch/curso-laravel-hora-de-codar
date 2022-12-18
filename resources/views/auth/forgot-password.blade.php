b7c5166ecb336330cfd72da5ea2ae87f0ed7e9d9
Wagner Chequeleiro Cordeiro <70072869+wagnerchc@users.noreply.github.com>
2022-12-18 10:37:39 -0300

    traducao-telas-login

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Esqueceu sua senha? Sem problemas. Basta nos informar seu endereço de e-mail e enviaremos um e-mail com um link de redefinição de senha que permitirá que você escolha um novo.') }}
         </div>
 
         @if (session('status'))
@@ -24,9 +24,9 @@
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-end mt-4 text-center">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Enviar Link de redefinição de senha de e-mail') }}
                 </x-jet-button>
             </div>
         </form>
